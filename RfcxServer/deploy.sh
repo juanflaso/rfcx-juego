0367df475c4b796606eae9a33291e875915aed6d
juanflaso <juanflaso1@hotmail.com>
2019-07-22 22:12:40 -0500

    sh files modified

--- RfcxServer/deploy.sh
+++ RfcxServer/deploy.sh
@@ -11,31 +11,13 @@ done;
 cd ./WebApplication
 dotnet restore
 dotnet publish -c Release -o $APP_DIR
-cp ./rfcx-espol-server.service /etc/systemd/system
+cp ./game-platform.service /etc/systemd/system
 cd ..
-systemctl stop rfcx-espol-server.service
-systemctl enable rfcx-espol-server.service
-systemctl start rfcx-espol-server.service
+systemctl stop game-platform.service
+systemctl enable game-platform.service
+systemctl start game-platform.service
 # systemctl status rfcx-espol-server.service
 
 chmod -R 755 $APP_DIR/resources
 chown -R $ICE_USER $APP_DIR/resources
 
-# Icecast
-cd ./Icecast
-cp ./refresh_playlists.sh $APP_DIR
-cp -r ./icecast-config $APP_DIR/
-chmod -R 777 $APP_DIR/'icecast-config'
-cp -r ./icecast-web $APP_DIR/
-chmod -R 777 $APP_DIR/'icecast-web'
-cp -r ./icecast-admin $APP_DIR/
-chmod -R 777 $APP_DIR/'icecast-admin'
-cp -r ./icecast-log $APP_DIR/
-chmod -R 777 $APP_DIR/'icecast-log'
-cp ./rfcx-espol-playlist.service /etc/systemd/system
-sed -i 's/ICE_USER/'$ICE_USER'/g' /etc/systemd/system/rfcx-espol-playlist.service
-cd ..
-systemctl stop rfcx-espol-playlist.service
-systemctl enable rfcx-espol-playlist.service
-systemctl start rfcx-espol-playlist.service
-# systemctl status rfcx-espol-playlist.service
