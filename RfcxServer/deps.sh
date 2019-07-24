fb3c89e947f87dff01402f3c744f565716a5bfdb
juanflaso <juanflaso1@hotmail.com>
2019-07-23 21:05:52 -0500

    service file and appsettings changed

--- RfcxServer/deps.sh
+++ RfcxServer/deps.sh
@@ -16,29 +16,3 @@ apt-get update
 apt-get install ffmpeg  
 apt-get install frei0r-plugins 
 
-# icecast
-
-apt-get install libxslt1-dev
-apt-get install libshout3-dev 
-apt-get install libvorbis-dev
-wget http://downloads.xiph.org/releases/icecast/icecast-2.4.3.tar.gz
-tar zxvf icecast-2.4.3.tar.gz
-rm icecast-2.4.3.tar.gz
-cd icecast-2.4.3
-./configure
-make
-make install
-cd ..
-# rm icecast-2.4.3.tar.gz
-
-#ices
-
-wget http://downloads.us.xiph.org/releases/ices/ices-2.0.2.tar.gz
-tar zxvf ices-2.0.2.tar.gz
-rm ices-2.0.2.tar.gz
-cd ices-2.0.2
-./configure
-make
-make install
-cd ..
-
